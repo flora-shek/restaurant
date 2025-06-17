1b8d4371504488651c51d8f8c5be270b6e7d02ef
florashek24 <florashek24@gmail.com>
2025-06-17 15:15:07 +0000

    updated app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="Attendance" options={{ title: 'Attendance' }} />
-        <Stack.Screen name="Billing" options={{ title: 'Billing' }} />
-        <Stack.Screen name="Orders" options={{ title: 'Orders' }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
